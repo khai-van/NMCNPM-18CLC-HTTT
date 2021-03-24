9a16bc75a54e7427ade673d17fab0b24c4edc710
18127114 <levankhai6729@gmail.com>
2021-03-24 10:42:19 +0700

    update trang thêm sản phẩm

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var path = require('path');
 var hbs = require('express-handlebars');
-var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var multer = require('multer');
 
 var admin = require('./routes/admin');
 var index = require('./routes/index');
@@ -13,8 +17,38 @@ app.engine('.handlebars', hbs({defaultLayout:'layout'}));
 app.set('view engine', '.handlebars');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
 
 app.use('/', index);
 app.use('/admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,66 @@
 var express = require('express');
+var multer = require('multer');
+var fs = require("fs");
+
 var router = express.Router();
 
+var upload = multer();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('admin', {layout: 'adminLayout'});
+router.get('/', function(req, res) {
+	res.render('admin', {layout: 'adminLayout'});
 });
 
-router.get('/addProduct', function(req, res, next) {
-  res.render('addProduct', {layout: 'adminLayout', productType: ["gundam","toys","game"]});
+router.get('/addProduct', function(req, res) {
+	res.render('addProduct', {layout: 'adminLayout', productType: ["gundam","toys","game"]});
 });
 
-router.post("/multiple-upload", function(req, res, next) {
-  res.render('addProduct', {layout: 'adminLayout'});
+router.post("/addProduct", upload.array('upload',10), function(req, res) {
+	var name = req.body.name;
+	var price = req.body.price;
+	var amount = req.body.amount;
+	var type = req.body.type;
+	var descript = req.body.descript;
+
+	req.checkBody('name', 'Chưa điền tên sản phẩm.').notEmpty();
+	req.checkBody('price', ' Chưa điền giá sản phẩm.').notEmpty();
+	req.checkBody('amount', 'Chưa điền số lượng.').notEmpty();
+	req.checkBody('type', 'Chưa chọn loại sản phẩm.').not().equals("0");
+	req.checkBody('descript', 'Chưa điền mô tả.').notEmpty();
+
+	var errors = req.validationErrors();
+	console.log(req.files);
+
+
+	if(req.files.length == 0){
+		errors.push({ param: 'file', msg: 'Không có file ảnh nào của sản phẩm.', value: ''})
+	}
+
+	if(errors){
+		res.render('addProduct',{
+			errors:errors, layout: 'adminLayout', productType: ["gundam","toys","game"]
+		});
+	}
+	else {
+		var dir = "./public/images/"+name;
+		
+		try {
+			if (!fs.existsSync(dir)) {
+				fs.mkdirSync(dir)
+			}
+		} catch (err) {
+			console.error(err)
+		}
+		for(var i=0; i < req.files.length;i++){
+			console.log(req.files[i]);
+			fs.writeFile(req.files[i].originalname, req.files[i].buffer,'binary', function(err) {
+				console.log(err);
+			});
+		}
+		res.render('addProduct',{
+			success: true, layout: 'adminLayout', productType: ["gundam","toys","game"]});
+	}
+	
+
 });
 module.exports = router;
